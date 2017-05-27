c1ef5ebee73cd4915996da0b396aa399d3f46080
Mingxing LAI <me@mingxinglai.com>
2017-05-27 09:48:54 +0800

    move class Storage to storage.py file

--- emcli/emcli.py
+++ emcli/emcli.py
@@ -11,29 +11,7 @@ import fileinput
 
 import yagmail
 
-
-class Storage(dict):
-    """
-    A Storage object is like a dictionary except `obj.foo` can be used
-    in addition to `obj['foo']`.
-    """
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError as k:
-            raise AttributeError(k)
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def __delattr__(self, key):
-        try:
-            del self[key]
-        except KeyError as k:
-            raise AttributeError(k)
-
-    def __repr__(self):
-        return '<Storage ' + dict.__repr__(self) + '>'
+from storage import Storage
 
 
 def _argparse():
---
+++ emcli/storage.py
@@ -0,0 +1,22 @@
+class Storage(dict):
+    """
+    A Storage object is like a dictionary except `obj.foo` can be used
+    in addition to `obj['foo']`.
+    """
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError as k:
+            raise AttributeError(k)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError as k:
+            raise AttributeError(k)
+
+    def __repr__(self):
+        return '<Storage ' + dict.__repr__(self) + '>'
